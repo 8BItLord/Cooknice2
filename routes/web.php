975b1ccfadf0981f26f0cd9f3065129733efac6a
MuhammadAbdurrafi <muhammad_abdurrafi@students.unnes.ac.id>
2025-06-08 12:22:51 +0700

    logout dan recipes controller

--- routes/web.php
+++ routes/web.php
@@ -82,3 +82,4 @@ Route::get('/auth/facebook/callback', [RegisterController::class, 'handleFaceboo
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
